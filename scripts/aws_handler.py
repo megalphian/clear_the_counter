16d78a24265981b09d90e2c9d870cf2f43c7b3f9
Megnath Ramesh <megnath@ualberta.ca>
2018-01-27 22:34:15 -0700

    F'n YAAAA BOIII

--- scripts/aws_handler.py
+++ scripts/aws_handler.py
@@ -12,6 +12,12 @@ bucket_name = 'hacked2018'
 filename = 'buffer.jpg'
 min_confidence = 70
 
+is_food=False
+is_dishes=False
+
+food_list = ['Food', 'Banana', 'Pasta']
+dish_list = ['Pot', 'Cup', 'Dish']
+
 def upload_to_s3():
     try:
         s3.upload_file(filename, bucket_name, filename)
@@ -30,6 +36,10 @@ def run_recognition(img):
     except:
         print('wtf rek')
 
+def reset_state():
+    is_food = False
+    is_dishes = False
+
 if __name__=="__main__":
     cam = cv2.VideoCapture(0)
     # while True:
@@ -39,7 +49,15 @@ if __name__=="__main__":
         upload_to_s3()
         # cv2.imshow('my webcam', img)
         response = run_recognition(img)
-        type(response)
+        print(len(response['Labels']))
+        for i in range(len(response['Labels'])):
+            if (response['Labels'][i]['Name'] in food_list) and (is_food == False):
+                is_food = True
+                print('FOOOOOD')
+            if (response['Labels'][i]['Name'] in dish_list) and (is_dishes == False):
+                is_dishes = True
+                print
+        reset_state()
     except Exception as ex:
         print('wtf')
         print(ex)
