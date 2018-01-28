2a4be5afda7606d0f5c18c8ac8f33e0416d81ff7
Megnath Ramesh <megnath@ualberta.ca>
2018-01-27 19:08:50 -0700

    Getting riggety riggety rekt son!

---
+++ scripts/aws_handler.py
@@ -0,0 +1,45 @@
+import sys
+import os
+import io
+
+import cv2
+import boto3
+
+from threading import Timer
+
+s3 = boto3.client('s3')
+rekt = boto3.client('rekognition')
+bucket_name = 'hacked2018'
+
+filename = 'buffer.jpg'
+min_confidence = 70
+
+def upload_to_s3():
+    try:
+        s3.upload_file(filename, bucket_name, filename)
+    except Exception as ex:
+        print('wtf s3')
+
+def run_recognition(img):
+    try:
+        response = rekt.detect_labels(Image={
+        "S3Object":{
+        "Bucket": bucket_name,
+        "Name": filename
+        }
+        }, MinConfidence=min_confidence)
+        print(response)
+    except:
+        print('wtf rek')
+
+cam = cv2.VideoCapture(0)
+# while True:
+try:
+    ret_val, img = cam.read()
+    cv2.imwrite(filename, img)
+    upload_to_s3()
+    # cv2.imshow('my webcam', img)
+    run_recognition(img)
+except Exception as ex:
+    print('wtf')
+    print(ex)
